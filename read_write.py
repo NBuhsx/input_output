f44a5965e7ef183bd63a666554955b32b4350470
NBuhsx <trubitsyn98@gmail.com>
2022-05-19 21:05:20 +0300

    add write_file, write_json

--- read_write.py
+++ read_write.py
@@ -1,12 +1,16 @@
-from sys import executable
+import json
 from more_itertools import islice_extended
 from pathlib import Path
 
-from typing import Callable, Generator, Any, Optional
+from typing import Callable, Generator, Any, Iterable, Optional, Literal
 
 
-def read_rows(pathfile:str|Path, templete:Optional[Callable[[str], Any]]=None, mode:str='r', 
-            islice:slice=slice(0,None,1), encoding:str='utf-8') -> Generator[str | Any, None, None]:
+def read_rows(
+        pathfile:str|Path, 
+        templete:Optional[Callable[[str], Any]]=None, 
+        mode:Literal['r', 'rb']='r', 
+        islice:slice=slice(0,None,1), 
+        encoding:str='utf-8') -> Generator[str | Any, None, None]:
     """Возращает генератор по строчно"""
     with open(file=pathfile, mode=mode, encoding=encoding) as file:
         for row in islice_extended(file)[islice]:
@@ -16,3 +20,27 @@ def read_rows(pathfile:str|Path, templete:Optional[Callable[[str], Any]]=None, m
                 break
             except:
                 continue
+
+
+def write_file(
+        pathfile:str|Path, 
+        write:Iterable|str, 
+        mode:Literal['a', 'w', 'wb']='a',
+        encoding:str='utf-8', 
+        addN:bool=False) -> None:
+    """ Записывает в файл"""
+    with open(file=pathfile, mode=mode, encoding=encoding) as file:
+        if isinstance(write, str):
+            file.write(write + '\n') if addN else file.write(write)
+        else:
+            file.writelines((str(elem + '\n') for elem in write)) if addN else file.writelines(write)
+
+
+def write_json(
+    pathfile:str, 
+    jsonObject:dict|list, 
+    ensure_ascii:bool=False, 
+    indent:int=3) -> None:
+    """ Пишет Json"""
+    with open(file=pathfile, mode='w', encoding='utf-8') as file:
+        json.dump(obj=jsonObject, fp=file, ensure_ascii=ensure_ascii, indent=indent)
