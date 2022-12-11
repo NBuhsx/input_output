167eb7be92a8ee93474224c0af2abab1342b62e4
NBuhsx <trubitsyn98@gmail.com>
2022-12-11 16:29:23 +0300

    refact setup.py

--- example.py
+++ example.py
@@ -1,15 +1,15 @@
 import re
-from my_input import my_input2
+from input_output.my_input import my_input2
 
 
 # str -> dict[str, str]
 # description: headers from Burp
-# result = dict(my_input2(
-#     execute_with_element=lambda row: re.split(': ', row)))
+result = dict(my_input2(
+    execute_with_element=lambda row: re.split(': ', row)))
 
 
 # str -> dict[str, str]
 # descripions: cookies from Burp
-# result = dict(my_input2(
-#     inside_string=lambda row: re.split("; ", row),
-#     execute_with_element=lambda row: re.split('=', row)))
+result = dict(my_input2(
+    inside_string=lambda row: re.split("; ", row),
+    execute_with_element=lambda row: re.split('=', row)))
--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 setup(
     name='input_output',
-    version='1.0',
+    version='1.0.0',
     description='Input output yield',
     author='NBuhsx',
     author_email='sergey33sergey@yandex.com',
-    packages=['input_output'],  # would be the same as name
-    # external packages acting as dependencies
-    install_requires=['wheel', 'bar', 'greek'],
+    url="https://notabug.org/NBuhsx/input_output",
+    packages=["input_output"],
+    install_requires=[
+        "more-itertools==9.0.0",
+    ],
 )
